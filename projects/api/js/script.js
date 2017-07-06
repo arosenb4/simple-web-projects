a78c76e4dfc1cfc0d403d662c9ad465c50661187
arosenb4 <arosenb4@vols.utk.edu>
2017-07-06 17:13:33 -0400

    css formatted for suggestion bar

--- projects/api/js/script.js
+++ projects/api/js/script.js
@@ -1,4 +1,5 @@
 //TODO: refactor code
+//    : remove id elements from functions
 availableTags = [
     "bubble sort",
     "cocktail shaker sort",
@@ -114,14 +115,17 @@ searchBar.setAvailableTags(availableTags);
 searchBar.toggleSuggestion("ON");
 
 $(searchBar.searchBtn).on("click", function(){
+     $("#ui-id-1").animate({ opacity : '0.0'}, 0);
       searchBar.getWikiRequest();
       searchBar.toggleResult("ON");
 });
 $(searchBar.searchBar).on("keyup", function(e){
+   $("#ui-id-1").animate({ opacity : '1.0'}, 300);
     searchBar.toggleResult("OFF");
     var key = e.which;
     var enter = 13, backspace = 8;
     if(key==enter){
+        $("#ui-id-1").animate({ opacity : '0.0'}, 0);
         searchBar.getWikiRequest();
         searchBar.toggleResult("ON");
        return;
